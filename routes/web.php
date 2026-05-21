3f5629bf91506f44fde1319eaf5bd5a9ad8bbe91
Turnionz <119740234+Turnionz@users.noreply.github.com>
2026-05-21 12:02:09 +0000

    middleware

--- routes/web.php
+++ routes/web.php
@@ -13,35 +13,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => to_route('trips.index'));
 
-Route::resource('trains', TrainController::class);
-Route::resource('trips', TripController::class);
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('register', [AuthController::class, 'create'])->name('register');
 
-Route::get('login', [AuthController::class, 'showLogin'])->name('login');
-Route::get('register', [AuthController::class, 'create'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('register', [AuthController::class, 'store'])->name('auth.register');
+});
 
-Route::post('login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('register', [AuthController::class, 'store'])->name('auth.register');
+Route::middleware('auth')->group(function () {
 
-Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
-Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
+    Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
+    Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
 
-Route::resource('employees', EmployeeController::class);
+    Route::resource('trains', TrainController::class);
+    Route::resource('trips', TripController::class);
+    Route::resource('employees', EmployeeController::class);
+    Route::resource('crews', CrewController::class);
+    Route::resource('wagons', WagonController::class);
+    Route::resource('stations', StationController::class);
+    Route::resource('routes', RouteController::class);
+    Route::resource('tickets', TicketController::class);
 
-Route::resource('crews', CrewController::class);
+    Route::post('/trips/{trip}/details', [TripController::class, 'details'])->name('trips.details');
+    Route::post('/trips/buy', [TicketController::class, 'buy'])->name('trips.buy');
+    Route::post('/trips/payment', [TicketController::class, 'store'])->name('trips.payment');
+    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
 
-Route::resource('wagons', WagonController::class);
-
-Route::resource('stations', StationController::class);
-
-Route::resource('routes', RouteController::class);
-
-Route::resource('tickets', TicketController::class);
-
-Route::post('/trips/{trip}/details', [TripController::class, 'details'])->name('trips.details');
-Route::post('/trips/buy', [TicketController::class, 'buy'])->name('trips.buy');
-Route::post('/trips/payment', [TicketController::class, 'store'])->name('trips.payment');
-
-Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
-
-Route::delete('deregNeighbour/{station}', [StationController::class, 'deregisterNeighbour'])->name('deregNeighbour');
-Route::put('registerNeighbour/{station}', [StationController::class, 'registerNeighbour'])->name('registerNeighbour');
+    Route::delete('deregNeighbour/{station}', [StationController::class, 'deregisterNeighbour'])->name('deregNeighbour');
+    Route::put('registerNeighbour/{station}', [StationController::class, 'registerNeighbour'])->name('registerNeighbour');
+});
